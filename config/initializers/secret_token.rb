52ae32ad6c4146f44178b99342d696891a8afed8
Stefan Portay <stefanportay@gmail.com>
2013-07-14 00:49:29 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mixrails::Application.config.secret_key_base = '32057e584f88a39bcceabcd7057165f9ceb09c3477135626c29390964fe09c9d5a1c87c1b344444ee5736424e14063eb823f13658cbb989647aa36431210e781'
